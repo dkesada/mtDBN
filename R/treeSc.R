a27d14708ca68e120193f6ecf84f1165feb4de8e
dkesada <dkesada@gmail.com>
2021-05-11 17:42:29 +0200

    finishing treeSc

--- R/treeSc.R
+++ R/treeSc.R
@@ -6,23 +6,41 @@
 treeSc <- R6::R6Class("treeSc",
     public = list(
       #' @description
-      #' Fit the model tree and each leaf's DBN
+      #' Create the tree scheme with a rpart tree
       #' @param tree the rpart tree that we want to get the structure from
-      #' @return A new 'causlist' object
-      define_tree = function(tree){
+      #' @return A new 'treeSc' object
+      initialize = function(tree){
         cuts <- labels(tree) # The bloody cuts are in the labels of the object. It took me a while to figure this one out
         cuts_e <- parse(text = cuts)
         nodes <- as.numeric(row.names(tree$frame)) # The nodes are in preorder
         depth <- private$tree_depth(nodes)
         e <- new.env()
         e$i <- 1
-
-
+        private$tree_sc <- private$add_node_rec(e, nodes, depth, cuts_e, "(root)")
       },
 
-      # Given an instance, return the appropriate model and the parent model
-      get_models = function(){
+      get_tree_sc = function(){
+        return(private$tree_sc)
+      },
 
+      # Given a dataset, return the appropriate model for each row
+      classify_dt = function(dt){
+        return(0)
+      },
+
+      #' @description
+      #' Given an instance, return the appropriate model and the parent model
+      #' @param inst a row from a data.table
+      #' @return
+      classify_inst = function(inst){
+        node_i <- private$tree_sc
+
+        while(!is.null(node_i$r_node) && !is.null(node_i$l_node)){ # While not in a leaf
+          if(nrow(inst[eval(node_i$l_node$cut_e)]))
+            node_i <- node_i$l_node
+        }
+
+        return(0)
       }
     ),
 
@@ -57,18 +75,25 @@ treeSc <- R6::R6Class("treeSc",
           return(curr_node_e)
 
         else if(depth[curr_i] < depth[e$i]) # Next node is a child. Left branch
-          curr_node_e$l_node <- add_node_rec(e, nodes, depth, cuts_e, curr_node_e)
+          curr_node_e$l_node <- private$add_node_rec(e, nodes, depth, cuts_e, curr_node_e)
 
         if(e$i > length(nodes)) # No more nodes after left branch
           return(curr_node_e)
 
         else if(depth[curr_i] < depth[e$i]) # Next node is a child. Right branch
-          curr_node_e$r_node <- add_node_rec(e, nodes, depth, cuts_e, curr_node_e)
+          curr_node_e$r_node <- private$add_node_rec(e, nodes, depth, cuts_e, curr_node_e)
 
         return(curr_node_e)
       },
 
-      create_node <- function(name, parent, cut_e){
+      #' @description
+      #' Defines a node in the tree scheme as an environment with a node name,
+      #' a left node, a right node, a parent node and a cut rule.
+      #' @param name the integer defining the name of the node
+      #' @param parent environment of the parent node
+      #' @param cut_e expression of the cut in the node
+      #' @return the new node environment
+      create_node = function(name, parent, cut_e){
         res <- new.env() # I could create an R6 class instead, but I'm feeling lazy
         res$name <- name
         res$l_node <- NULL
